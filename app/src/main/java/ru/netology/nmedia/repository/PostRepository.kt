07305afcc868a8479d455d76f4aa89091f8feea6
Anton Bolotin <a.y.bolotin@gmail.com>
2024-08-05 17:49:11 +0300

    12. Flow (update)

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,13 +1,15 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    val data: LiveData<List<Post>>
+    val data: Flow<List<Post>>
     suspend fun getAll();
     suspend fun likeByMe(id: Long)
     suspend fun unlikeByMe(id: Long)
     suspend fun remove(id: Long, localId: Long)
     suspend fun save(post: Post)
+    fun getNewerCount(id: Long) : Flow<Int>
+    suspend fun setAllViewed()
 }
